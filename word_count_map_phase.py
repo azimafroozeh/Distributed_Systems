27b0b684e35109d7998ea62740be97db561de0b8
Azim Afroozeh <afroozeh3@gmail.com>
2018-12-03 00:20:51 +0100

    word_count_map_phase fixed

--- word_count_map_phase.py
+++ word_count_map_phase.py
@@ -7,26 +7,29 @@ def word_count_map(split_number, worker_id):
         text = f.read()
     text = text.lower()
     # split returns a list of words delimited by sequences of whitespace (including tabs, newlines, etc, like re's \s)
-    word_list = text.split()
+    key_values = {}
+    # for key in text.split():
+       # key_values[key] = 1;
     # word_list=map(lambda x:x+'1',word_list)
 
-    key_values = Counter(word_list).most_common()
+    #key_values = Counter(word_list).most_common()
     f0 = open(path + "worker" + str(worker_id) + "/partition0" + "/key_values_split_" + str(split_number) + ".txt", 'w')
     f1 = open(path + "worker" + str(worker_id) + "/partition1" + "/key_values_split_" + str(split_number) + ".txt", 'w')
-    for key, value in key_values:
+
+    for key in text.split():
         hash_object = hashlib.md5(bytes(key, 'utf-8'))
         if (int(hash_object.hexdigest(), 16) % 2) == 0:
             writer = csv.writer(f0, delimiter='\t')
-            writer.writerow([key] + [value])
+            writer.writerow([key] + [1])
         else:
             writer = csv.writer(f1, delimiter='\t')
-            writer.writerow([key] + [value])
+            writer.writerow([key] + [1])
 
 
-"""
+
 j = 0
 while j < 9:
     for i in range(1, 4):
         word_count_map(j, i)
         j += 1
-"""
+
--- word_count_reduce_phase.py
+++ word_count_reduce_phase.py
@@ -1,7 +1,28 @@
-def word_count_reduce(workers, partition):
-    for worker in workers:
-        path = worker.path + "/partition_"+  str(partition)
-        data = read_all_csv(path)
+def word_count_reduce():
+    #for worker in workers:
+        #path = worker.path + "/partition_"+  str(partition)
+        data = read_all_csv("intermediate_data\\text\partition0")
         # produce final result
 
 
+
+def read_all_csv(path):
+    import csv
+    import os
+    files=os.listdir(path)
+    files=[path+'/'+f for f in files]
+    result=[]
+    for file in files:
+        reader=csv.reader(open(file),delimiter='\t')
+        result.extend([row for row in reader])
+    output={}
+    for row in result:
+        if row[0] in output:
+            output[row[0]]+=row[1]
+        else:
+            output[row[0]]= row[1]
+    return output
+# reader=csv.reader(open('intermediate_data\\text\partition0\key_values_split_0.txt'),delimiter='\t')
+# for row in reader:
+#     print(row)
+#print(read_all_csv("intermediate_data\\text\partition0"))
