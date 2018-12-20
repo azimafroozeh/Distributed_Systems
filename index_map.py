ddbf5316ed79f1cd2b1f0e747c456659b85aa45f
AdolphusEnvy <hy8227852@163.com>
2018-12-20 23:34:51 +0100

    index

---
+++ index_map.py
@@ -0,0 +1,30 @@
+def index_map(split_number, worker_id):
+    from random import randint
+    import time
+    # time.sleep(randint(0, 9))
+    print("running")
+    import csv
+    from collections import Counter
+    import hashlib
+    path = "/efs/"
+    with open(path + "input/" + str(split_number) + ".txt", 'r') as f:
+        text = f.read()
+    text = text.lower()
+
+    path1 = "/home/ec2-user/"
+    # key_values = Counter(word_list).most_common()
+    f0 = open(path1 + "worker_" + str(worker_id) + "intermediate_result/partition_0" + "/key_values_split" + str(
+        split_number) + ".txt", 'w')
+    f1 = open(path1 + "worker_" + str(worker_id) + "intermediate_result/partition_1" + "/key_values_split" + str(
+        split_number) + ".txt", 'w')
+
+    for key in text.split():
+        hash_object = hashlib.md5(bytes(key, 'utf-8'))
+        if (int(hash_object.hexdigest(), 16) % 2) == 0:
+            writer = csv.writer(f0, delimiter='\t')
+            writer.writerow([key] + [split_number])
+        else:
+            writer = csv.writer(f1, delimiter='\t')
+            writer.writerow([key] + [split_number])
+
+    return "done11"
