abed50c9ce83adcc9a8386928cdf93ae8fe1c9bb
AdolphusEnvy <hy8227852@163.com>
2018-12-21 07:44:25 +0100

    directly read

--- word_count_reduce_phase.py
+++ word_count_reduce_phase.py
@@ -1,54 +1,84 @@
-import rpyc
 def word_count_reduce(workers, partition):
+    print("reduce task")
+    print("test")
     import rpyc
-    print("dgdgd")
+    import csv
+    import os
+    import sys
+    # import requests
+    data = []
+    result = {}
+    print(partition)
+    print(workers)
+    # response  = requests.get('http://localhost:8000/key_values_split_8.txt')
+    # print(response.text)
     for worker in workers:
-        path = worker.path + "/partition_"+  str(partition)
+        print(worker)
+        path = worker.path + "/partition_" + str(partition)
+        print(path)
         try:
-            conn = rpyc.classic.connect("localhost", worker.port_number)
+            conn = rpyc.classic.connect(worker.ip, port=22222)
         except:
             print("Ddddddddddddddddddddddddddddd")
         else:
-            #remote_func = conn.namespace['read_all_csv']
-            data.extend(read_all_csv(path,conn))
-    result = {}
-    for key, value in data:
-        if key in result:
-            result[key] += 1
-        else:
-            result[key] = 1
-    print(result)
+            print("sfasfa")
+            sum = 0
+            #remote_func = worker.conn.namespace['read_all_csv']
+            try:
+                data1 = read_all_csv(path,conn)
+                print("before")
+                print("data1")
+                a = str(data1)
+                data2 = eval(a)
+                print("after")
+                # print(type(data1))
+                # a = str(data1)
+                # print(eval(a))
+                # text_file = open("Output.txt", "w")
+                # text_file.write(a)
+                # text_file.close()
+                for d in data2:
+                    if (d is None):
+                        print("dddddddddddddddddD")
+                        continue
+                    # print(str(d))
+                    # print(d)
+                    sum += len(str(d))
+                    # print(sum)
+                    key = d[0]
+                    print(d[0])
+                    data = d[1]
+                    print(d[1])
+                    if key in result:
+                        result[key] += 1
+                    else:
+                        result[key] = 1
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
 
-
-
-        # produce final result
-reduce_task_txt = """
-def hello1():
-    print("reduce task")
-"""
-def hello3():
-    print("hello3")
+                # print(result)
+    print("vb")
+    f = open("/home/ec2-user/partition_" + str(partition), 'w')
+    f.write(str(result))
+    f.close()
+    return ("dddddddooooooneee")
 
 
 def read_all_csv(path,conn):
+    print("sending infromatiosn")
     import csv
     import os
-    files = os.listdir(path)
-    files = [path+'/'+f for f in files]
+    print(path)
+    files =conn.modules.os.listdir(path)
+    files = [path + '/' + f for f in files]
+    # print(files)
     result = []
     for file in files:
+        print(file)
         reader = csv.reader(conn.builtins.open(file), delimiter='\t')
         result.extend([row for row in reader])
+
+    # print(result)
     return result
-# reader=csv.reader(open('intermediate_data\\text\partition0\key_values_split_0.txt'),delimiter='\t')
-# for row in reader:
-#     print(row)
-#print(read_all_csv("/Users/azimafroozeh/PycharmProjects/DistributedSystem/worker1/partition0"))
-#word_count_reduce()
-try:
-    conn1 = rpyc.classic.connect("localhost", port=22221)
-except:
-    print("Ddddddddddddddddddddddddddddd")
-else:
-    conn1.execute(reduce_task_txt)
-    remote_func = conn1.namespace['hello1']
